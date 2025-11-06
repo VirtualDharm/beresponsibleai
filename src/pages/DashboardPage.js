bb5c8b24041e3a5b821b95fd6b58bb8088939e40
DESKTOP-P82J9G2\dharm <mdharm4air.fm@gmail.com>
2025-11-06 12:30:40 +0530

    Dashboard componenets used now

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
+import Dashboard from '../components/AfterLogin/Dashboard';
 
 const DashboardPage = () => {
-  return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold font-brockmann">Welcome to the Dashboard</h1>
-      <p className="mt-4 text-lg text-gray-400">You are now logged in.</p>
-      {/* You can build out your dashboard components here */}
-    </div>
-  );
+  return <Dashboard />;
 };
 
 export default DashboardPage;
